6a02453939023f1302b2afaf9e6ebf91ebd099ba
Guruprasad Kulkarni <guru@linux.com>
2022-05-09 07:46:08 +0200

    added result constraints

--- src/main/kotlin/com/linux/simplemathtest/MathTestController.kt
+++ src/main/kotlin/com/linux/simplemathtest/MathTestController.kt
@@ -1,10 +1,9 @@
 package com.linux.simplemathtest
 
+import javafx.collections.ObservableList
 import javafx.event.ActionEvent
-import javafx.event.EventHandler
 import javafx.fxml.FXML
 import javafx.scene.control.*
-import java.lang.Thread.sleep
 import java.util.concurrent.TimeUnit
 import kotlin.random.Random
 import kotlin.system.exitProcess
@@ -13,29 +12,43 @@ import kotlin.system.exitProcess
 enum class Operation(val notation: String) {
     ADDITION("+") {
         override fun calculate(lhs: Int, rhs: Int): Int = lhs + rhs
+        override fun isValid(lhs: Int, rhs: Int, maxResult: Int): Boolean = calculate(lhs, rhs) <= maxResult
     },
     SUBTRACTION("-") {
         override fun calculate(lhs: Int, rhs: Int): Int = lhs - rhs
+        override fun isValid(lhs: Int, rhs: Int, maxResult: Int): Boolean = lhs >= rhs && calculate(lhs, rhs) >= 0
     },
     MULTIPLICATION("*") {
         override fun calculate(lhs: Int, rhs: Int): Int = lhs * rhs
+        override fun isValid(lhs: Int, rhs: Int, maxResult: Int): Boolean = calculate(lhs, rhs) <= maxResult
     },
     DIVISION("/") {
         override fun calculate(lhs: Int, rhs: Int): Int = lhs / rhs
+        override fun isValid(lhs: Int, rhs: Int, maxResult: Int): Boolean = lhs % rhs == 0
     };
 
     abstract fun calculate(lhs: Int, rhs: Int): Int
+    abstract fun isValid(lhs: Int, rhs: Int, maxResult: Int): Boolean
 }
 
 data class ValueLimits(var min: Int = 0, var max: Int = 100)
 data class AllowedOperations(var addition: Boolean = true, var subtraction: Boolean = false, var multiplication: Boolean = false, var division: Boolean = false) {
-    fun randomOperation(lhs: Int, rhs: Int): OperationWithTimedResult {
+    fun randomOperation(limits: ValueLimits): OperationWithTimedResult {
         val operations: MutableList<Operation> = mutableListOf()
         if (addition) operations.add(Operation.ADDITION)
         if (subtraction) operations.add(Operation.SUBTRACTION)
         if (multiplication) operations.add(Operation.MULTIPLICATION)
         if (division) operations.add(Operation.DIVISION)
-        val operation = operations[Random.nextInt(operations.size)]
+
+        var operation: Operation
+        var lhs: Int
+        var rhs: Int
+        do {
+            operation = operations[Random.nextInt(operations.size)]
+            lhs = Random.nextInt(limits.min, limits.max)
+            rhs = Random.nextInt(limits.min, limits.max)
+        } while (!operation.isValid(lhs, rhs, limits.max))
+
         return OperationWithTimedResult(lhs.toString(), operation.notation, rhs.toString(), operation.calculate(lhs, rhs).toString())
     }
 }
@@ -43,6 +56,29 @@ data class AllowedOperations(var addition: Boolean = true, var subtraction: Bool
 data class OperationWithTimedResult(val lhs: String, val operation: String, val rhs: String, val result: String, private val startTime: Long = System.currentTimeMillis()) {
     fun tookTime(): Long = TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis() - startTime)
     fun prettify(inputValue: String): String = "$lhs $operation $rhs = $result took ${tookTime()}s to enter $inputValue"
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as OperationWithTimedResult
+
+        if (lhs != other.lhs) return false
+        if (operation != other.operation) return false
+        if (rhs != other.rhs) return false
+        if (result != other.result) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result1 = lhs.hashCode()
+        result1 = 31 * result1 + operation.hashCode()
+        result1 = 31 * result1 + rhs.hashCode()
+        result1 = 31 * result1 + result.hashCode()
+        return result1
+    }
+
+
 }
 
 class MathTestController {
@@ -82,7 +118,6 @@ class MathTestController {
 
     private val allowedOperations = AllowedOperations()
     private val limits = ValueLimits()
-    private var generateNextOnCheck = false
 
     fun onAdditionChecked(actionEvent: ActionEvent) {
         allowedOperations.addition = (actionEvent.target as CheckBox).isSelected
@@ -110,11 +145,10 @@ class MathTestController {
         maxValue.text = limits.max.toString()
     }
 
-
     private lateinit var currentOperation: OperationWithTimedResult
 
     fun generate() {
-        currentOperation = allowedOperations.randomOperation(Random.nextInt(limits.min, limits.max), Random.nextInt(limits.min, limits.max))
+        currentOperation = allowedOperations.randomOperation(limits)
         lhs.text = currentOperation.lhs
         rhs.text = currentOperation.rhs
         operation.text = currentOperation.operation
@@ -122,30 +156,31 @@ class MathTestController {
         answerInput.text = ""
         answer.isVisible = false
         duration.isVisible = false
+
+        minValue.text = limits.min.toString()
+        maxValue.text = limits.max.toString()
     }
 
     fun onRevealAnswer() {
-        if (generateNextOnCheck) {
-            revealAnswer.onAction = EventHandler { sleep(1000); generate() }
-        }
         answer.isVisible = true
         duration.isVisible = true
         duration.text = "took ${currentOperation.tookTime()}s"
+
         if (answerInput.text == currentOperation.result) {
             answer.textFill = javafx.scene.paint.Color.GREEN
             duration.textFill = javafx.scene.paint.Color.GREEN
-            correctHistory.items.add(Label(currentOperation.prettify(answerInput.text)))
+            correctHistory.items.addIfNotExists(Label(currentOperation.prettify(answerInput.text)))
         } else {
             answer.textFill = javafx.scene.paint.Color.RED
             duration.textFill = javafx.scene.paint.Color.RED
-            incorrectHistory.items.add(Label(currentOperation.prettify(answerInput.text)))
+            incorrectHistory.items.addIfNotExists(Label(currentOperation.prettify(answerInput.text)))
         }
-
-
     }
 
-    fun onGenerateNextOnCheck(actionEvent: ActionEvent) {
-        generateNextOnCheck = (actionEvent.target as CheckBox).isSelected
+    fun ObservableList<Any>.addIfNotExists(label: Label) {
+        if (filterIsInstance<Label>().any { it.text == label.text }.not()) {
+            add(label)
+        }
     }
 
     fun onQuit() {
--- src/main/kotlin/com/linux/simplemathtest/SimpleMathTest.kt
+++ src/main/kotlin/com/linux/simplemathtest/SimpleMathTest.kt
@@ -8,7 +8,7 @@ import javafx.stage.Stage
 class SimpleMathTest : Application() {
     override fun start(stage: Stage) {
         val fxmlLoader = FXMLLoader(SimpleMathTest::class.java.getResource("simple-math-test.fxml"))
-        val scene = Scene(fxmlLoader.load(), 650.0, 400.0)
+        val scene = Scene(fxmlLoader.load(), 655.0, 395.0)
         stage.title = "Simple Maths Tester!"
         stage.scene = scene
         stage.isResizable = false
